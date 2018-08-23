91221987ecc6ae13b1b68beda79ad7a0fb3f02b6
Aleksander Szczęsny <aleksander03@gmail.com>
2018-08-23 14:07:36 +0200

    tiles in various colors, js tiles generation

---
+++ static/js/tilesGenerator.js
@@ -0,0 +1,31 @@
+
+
+
+function generateTiles() {
+    var i;
+    for (i = 2; i<85; i++ ) {
+        var firstTile = document.getElementById('1');
+        var copy = firstTile.cloneNode(true);
+        var insertPlace = document.getElementById('toInsertTiles');
+        copy.setAttribute('id', i);
+        if (i % 2 == 0) {
+            copy.setAttribute('class', 'everySecond');
+        } 
+        if (i % 3 == 0) {
+            copy.setAttribute('class', 'everyThird');
+        } 
+        if (i % 5 == 0) {
+            copy.setAttribute('class', 'everyFifth');
+        }
+
+
+
+
+
+        insertPlace.appendChild(copy);
+    }
+    
+
+}
+
+generateTiles();

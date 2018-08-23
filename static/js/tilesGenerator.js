52b9c3e94f4d6635e41fcc83885d9b0b208e2f59
Aleksander Szczęsny <aleksander03@gmail.com>
2018-08-24 01:00:48 +0200

    addedwhat may seem to some as bathtub with water

--- static/js/tilesGenerator.js
+++ static/js/tilesGenerator.js
@@ -1,22 +1,20 @@
 
-
-
-function generateTiles() {
+function generateTiles () {
     var i;
-    for (i = 2; i<85; i++ ) {
+    for (i = 2; i < 61; i++) {
         var firstTile = document.getElementById('1');
         var copy = firstTile.cloneNode(true);
         var insertPlace = document.getElementById('toInsertTiles');
         copy.setAttribute('id', i);
         if (i % 2 == 0) {
             copy.setAttribute('class', 'everySecond');
-        } 
-        if (i % 3 == 0) {
-            copy.setAttribute('class', 'everyThird');
-        } 
-        if (i % 5 == 0) {
-            copy.setAttribute('class', 'everyFifth');
         }
+        if (i % 3 == 0) {
+      copy.setAttribute('class', 'everyThird');
+    }
+        if (i % 5 == 0) {
+      copy.setAttribute('class', 'everyFifth');
+    }
 
 
 
@@ -24,7 +22,7 @@ function generateTiles() {
 
         insertPlace.appendChild(copy);
     }
-    
+
 
 }
 
